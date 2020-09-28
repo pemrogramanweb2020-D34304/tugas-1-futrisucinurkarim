f6a69ac38617fd5846d33784093b1a1633d92107
futrisucinurkarim <71260833+futrisucinurkarim@users.noreply.github.com>
2020-09-28 23:22:34 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,1 +1,26 @@
-
+<?php
+if ($_GET):
+    $bilangan1 = $_GET['bilangan1'];
+    $bilangan2 = $_GET['bilangan2'];
+    $operasi = $_GET['operasi'];
+    
+    switch ($operasi) {
+        case 'tambah':
+            $hasil = $bilangan1 + $bilangan2;
+            break;
+        case 'kurang':
+            $hasil = $bilangan1 - $bilangan2;
+            break;
+        case 'kali':
+            $hasil = $bilangan1 * $bilangan2;
+            break;
+        case 'bagi':
+            $hasil = $bilangan1 / $bilangan2;
+            break;
+    }
+ ?>
+       <br>
+        Hasil: <strong><?php echo $hasil ?></strong>
+      
+        <?php
+    endif; ?>
